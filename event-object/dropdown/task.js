404f877bb4c1a2ae2194bd8457d3824e3eeb66eb
KaPuHKa <p4elkaachen@mail.ru>
2022-08-19 22:49:12 +0300

    href not removed

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -10,17 +10,13 @@ function dropdown(){
         name.replace('dropdown__list_active', ''): name + ' dropdown__list_active';
 }
 
-// change value
-let links = document.querySelectorAll(".dropdown__link");
-links = Array.from(links).map(item => item.removeAttribute('href'));
-
-
 let items= document.querySelectorAll(".dropdown__item");
-items = Array.from(items).forEach(item => item.addEventListener('click', changevalue));
+items = Array.from(items).forEach(item => 
+    item.addEventListener('click', (event) => changevalue(event, item.textContent)));
 
-function changevalue(){
-    value.textContent = this.textContent;
+function changevalue(event, txt){
+    value.textContent = txt; 
     dropdown();
+    event.preventDefault();
 }
-
-// more as one 
+ 
