ef3d7ec28019076a4ac553080b91420ad6e65d9e
KaPuHKa <p4elkaachen@mail.ru>
2022-08-25 21:13:57 +0300

    add task via button

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,22 +3,13 @@ let inputTxt = document.getElementById('task__input');
 let addTxt = document.getElementById('tasks__add');
 let listTxt = document.getElementById('tasks__list');
 
-/*
+
 addTxt.addEventListener('click', (event) => {
     event.preventDefault();
-    send(inputTxt);
-    inputTxt.value = '';
-}) */
-
-
-inputTxt.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        if (inputTxt.value.trim().length > 0)
+    if (inputTxt.value.trim().length > 0)
             send(inputTxt);
         inputTxt.value = '';
-    }
-})
+}) 
 
 
 function send(ele){
