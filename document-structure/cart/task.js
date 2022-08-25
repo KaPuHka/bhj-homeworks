b1b18c04b0af254e4009067c1c8b3a315d82055f
KaPuHKa <p4elkaachen@mail.ru>
2022-08-25 08:15:33 +0300

    min 1, 0 impossible!

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -4,14 +4,14 @@ counts.forEach(count =>
     count.addEventListener('click', (event)=>{
         let changeButtons = Array.from(count.classList);
         let product = count.closest(".product");
-        let x = product.getElementsByClassName("product__quantity-value")[0];
-        quantity = x.innerText;
+        let quantityEle = product.getElementsByClassName("product__quantity-value")[0];
+        quantity = quantityEle.innerText;
 
         if (changeButtons.includes("product__quantity-control_inc"))
             quantity++;
-        if (changeButtons.includes("product__quantity-control_dec") && quantity>0)
+        if (changeButtons.includes("product__quantity-control_dec") && quantity>1)
             quantity--;   
-        x.innerText = quantity;
+        quantityEle.innerText = quantity;
 }))
 
 let addButtons = document.getElementsByClassName('product__add');
@@ -25,9 +25,9 @@ addButtons.forEach(adb =>
 let cart = document.querySelector('.cart__products');
 function addToCart(product) {
     let id = product.getAttribute("data-id");
-    let x = product.getElementsByClassName("product__quantity-value")[0];
-    let quantity = x.innerText;
-    x.innerText = 1;
+    let quantityEle = product.getElementsByClassName("product__quantity-value")[0];
+    let quantity = quantityEle.innerText;
+    quantityEle.innerText = 1;
     if (quantity > 0) {
         let alreadyInCart = document.getElementById(id);
         if ( !alreadyInCart) {
@@ -38,8 +38,8 @@ function addToCart(product) {
                         </div>`;
             cart.insertAdjacentHTML('beforeEnd', inner);
         } else {
-            x = alreadyInCart.getElementsByClassName("cart__product-count")[0]; 
-            x.innerText = parseInt(quantity) +parseInt(x.innerText);
+            quantityEle = alreadyInCart.getElementsByClassName("cart__product-count")[0]; 
+            quantityEle.innerText = parseInt(quantity) +parseInt(quantityEle.innerText);
         }
     }
     
