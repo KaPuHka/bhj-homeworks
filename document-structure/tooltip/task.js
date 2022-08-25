2ced157728ed76fb8c0aa548397e0fa4ec7d4a6e
KaPuHKa <p4elkaachen@mail.ru>
2022-08-25 08:07:34 +0300

    tooltip corr.

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,23 +3,22 @@ let textsWithTips = document.getElementsByClassName("has-tooltip");
 textsWithTips = Array.from(textsWithTips);
 
 textsWithTips.forEach((element, idx)=> {
-    let tip = element.getAttribute("title");
-    let posLeft = element.offsetLeft;
-    let posTop = element.offsetTop + 20;
-    let id = "tooltip-"+idx;
-    element.removeAttribute("title");
-    let inner = `<div class="tooltip" id="`+id+`" style="left: `+posLeft+`px; top: `+posTop+`px">`
-                                + tip +
-                          `</div> `;
-                    
-    element.insertAdjacentHTML('beforeEnd', inner);
     element.addEventListener('click', (event) => {
         event.preventDefault();
-        
+        let tip = element.getAttribute("title");
+        let posLeft = element.offsetLeft;
+        let posTop = element.offsetTop + 20;
+        let id = "tooltip-id"+idx;
+        element.removeAttribute("title");
+        let inner = `<div class="tooltip" id="`+id+`" style="left: `+posLeft+`px; top: `+posTop+`px">`
+                                + tip +
+                        `</div> `;
+                    
+        element.insertAdjacentHTML('beforeEnd', inner);
         let txt = document.getElementById(id);
         if (!txt.className.includes('tooltip_active'))
             txt.classList.add('tooltip_active')
         else
             txt.classList.remove('tooltip_active');
     })
-});
+})
